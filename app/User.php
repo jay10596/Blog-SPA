a91256bdc0cadd1b0c9efa5e9d0e6166cec2e84f
Jay10596 <jay.modi105@gmail.com>
2020-04-25 02:27:42 +1000

    Minor big fixes

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
 
 
     protected $fillable = [
-        'name', 'email', 'password', 'birthday', 'avatar'
+        'name', 'email', 'password', 'birthday'
     ];
 
     protected $hidden = [
